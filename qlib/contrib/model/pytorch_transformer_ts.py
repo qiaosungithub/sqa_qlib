ab3e686beff06f5f9ceb4e0db89ae70b7991ae6d
sqa-abroad <jzc_2007@mit.edu>
2025-05-29 12:20:36 +0000

    add comments

--- qlib/contrib/model/pytorch_transformer_ts.py
+++ qlib/contrib/model/pytorch_transformer_ts.py
@@ -223,11 +223,11 @@ class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=1000):
         super(PositionalEncoding, self).__init__()
         pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1) # [max_len, 1]
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # [d_model/2]
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
+        pe = pe.unsqueeze(0).transpose(0, 1) # [max_len, 1, d_model]
         self.register_buffer("pe", pe)
 
     def forward(self, x):
@@ -236,11 +236,11 @@ class PositionalEncoding(nn.Module):
 
 
 class Transformer(nn.Module):
-    def __init__(self, d_feat=6, d_model=8, nhead=4, num_layers=2, dropout=0.5, device=None):
+    def __init__(self, d_feat=6, d_model=8, nhead=2, num_layers=2, dropout=0.0, device=None):
         super(Transformer, self).__init__()
         self.feature_layer = nn.Linear(d_feat, d_model)
         self.pos_encoder = PositionalEncoding(d_model)
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dropout=dropout)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dropout=dropout) # TODO: activation is default relu
         self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_layers)
         self.decoder_layer = nn.Linear(d_model, 1)
         self.device = device
